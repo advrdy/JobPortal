beca003c099dd743fe254b9b4292b7b6e492eee3
advrdy <advrx88@gmail.com>
2024-10-31 21:27:50 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://jobportal-1xf0.onrender.com",
   credentials: true,
 };
 
